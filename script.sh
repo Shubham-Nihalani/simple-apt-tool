49b1bf1c35ff041e923eb823e50d864d7c3ba2b3
Shubham-Nihalani <shubhamnihalani09@gmail.com>
2025-06-10 19:01:04 +0000

    Added comments

--- script.sh
+++ script.sh
@@ -7,9 +7,11 @@ sleep 2
 echo "Do you want to install a package or remove it?"
 sleep 2
 
-read -n 2 -p "To install, press I. To remove, press R :" prompt1
-prompt="${prompt1^^}"
+read -n 2 -p "To install, press I. To remove, press R :" prompt1 # here 2 because of enter key
+prompt="${prompt1^^}" # always take prompt in uppercase
 echo -e "\n You entered: $prompt"
+
+# nested if-else to check prompts
 
 if [[ "$prompt" == "I" || "$prompt" == "R" ]]; then
     read -p "Type the package name: " pkg
@@ -22,10 +24,12 @@ if [[ "$prompt" == "I" || "$prompt" == "R" ]]; then
     sleep 2
     fi
 
+	# another if-else to install or remove packages
+
     if [[ "$prompt" == "I" ]]; then
         echo "Installing $pkg..."
         sleep 2
-        sudo apt install "$pkg"
+        sudo apt install "$pkg" 
         echo "$pkg installation complete. Exit code : $?"
     elif [[ "$prompt" == "R" ]]; then
         echo "Removing $pkg..."
@@ -35,4 +39,5 @@ if [[ "$prompt" == "I" || "$prompt" == "R" ]]; then
     fi
 else
     echo "Bhaggg...."
+	# case of invalud condition
 fi
